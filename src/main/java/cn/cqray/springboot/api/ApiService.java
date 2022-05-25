8f8b7f9e90f77c1efb7256fe46df2a95f486c092
PC <168169332@qq.com>
2022-05-25 11:23:31 +0800

    initial commit

--- src/main/java/cn/cqray/springboot/api/ApiService.java
+++ src/main/java/cn/cqray/springboot/api/ApiService.java
@@ -1,12 +1,9 @@
 package cn.cqray.springboot.api;
 
 import cn.cqray.springboot.api.dto.PageDto;
-import cn.cqray.springboot.api.redis.RedisService;
 import cn.cqray.springboot.api.response.ResponseService;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.data.redis.core.HashOperations;
-import org.springframework.data.redis.core.ListOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -28,8 +25,6 @@ import java.util.Set;
 public class ApiService {
 
     private final ApiConfiguration apiConfiguration;
-    /** Redis服务 **/ @Getter
-    private final RedisService redisService;
     /** 接口响应服务 **/ @Getter
     private final ResponseService responseService;
     /** 接口参数验证 **/
@@ -108,58 +103,6 @@ public class ApiService {
 
     public <T> T succeedWithPage(List<?>  data, String message) {
         return responseService.succeedWithPage(data, message);
-    }
-
-    public String lockCache(String key, String value, long timeout) {
-        return redisService.lock(key, value, timeout);
-    }
-
-    public String tryLockCache(String key, String value, long timeout) {
-        return redisService.tryLock(key, value, timeout);
-    }
-
-    public void unlockCache(String key, String token) {
-        redisService.unlock(key, token);
-    }
-
-    public boolean hasCache(String... keys) {
-        return redisService.hasKey(keys);
-    }
-
-    public void deleteCache(String... keys) {
-        redisService.delete(keys);
-    }
-
-    public void setCache(String key, Object value) {
-        redisService.set(key, value);
-    }
-
-    public void setCache(String key, Object value, long expireTime) {
-        redisService.set(key, value, expireTime);
-    }
-
-    public <T> T getCache(String key) {
-        return redisService.get(key);
-    }
-
-    public void setCacheExpire(String key, long expire) {
-        redisService.expire(key, expire);
-    }
-
-    public long getCacheExpire(String key) {
-        return redisService.getExpire(key);
-    }
-
-    public long setCacheAdditon(String key, long addition) {
-        return redisService.add(key, addition);
-    }
-
-    public HashOperations<String, String, Object> getCacheHash() {
-        return redisService.getHash();
-    }
-
-    public ListOperations<String, Object> getCacheList() {
-        return redisService.getList();
     }
 }
 
